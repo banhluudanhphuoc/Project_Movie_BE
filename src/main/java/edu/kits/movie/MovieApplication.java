bf11e2bfe8869ec88f7ebe57915a9863b1b8bd4f
Sỳ Nhật Phú <synhatphu2@gmail.com>
2023-08-09 16:29:45 +0700

    update

--- src/main/java/edu/kits/movie/MovieApplication.java
+++ src/main/java/edu/kits/movie/MovieApplication.java
@@ -1,10 +1,14 @@
 package edu.kits.movie;
 
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.io.ClassPathResource;
 
 @OpenAPIDefinition(info = @Info(title = "API for project", version = "v1.0.0"),
         servers = {
@@ -20,6 +24,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MovieApplication {
     public static void main(String[] args) {
         SpringApplication.run(MovieApplication.class, args);
+        ClassPathResource serviceAccount = new ClassPathResource(
+                "movie-1fb03-firebase-adminsdk-c1twk-7f70b5fa97.json");
+        try{
+            FirebaseOptions options = new FirebaseOptions.Builder()
+                    .setCredentials(GoogleCredentials.fromStream(serviceAccount.getInputStream()))
+                    .setStorageBucket("bonik-f7b39.appspot.com")
+                    .build();
+            FirebaseApp.initializeApp(options);
+        }catch(Exception e){
+            e.printStackTrace();
+        }
     }
 //    @Bean
 //    CommandLineRunner init(FileStorageService storageService) {
--- src/main/java/edu/kits/movie/Service/Impl/FileStorageServiceImpl.java
+++ src/main/java/edu/kits/movie/Service/Impl/FileStorageServiceImpl.java
@@ -1,11 +1,20 @@
 package edu.kits.movie.Service.Impl;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.*;
 import java.util.UUID;
 import java.util.stream.Stream;
 
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.storage.BlobId;
+import com.google.cloud.storage.BlobInfo;
+import com.google.cloud.storage.Storage;
+import com.google.cloud.storage.StorageOptions;
 import edu.kits.movie.Service.FileStorageService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.io.ClassPathResource;
@@ -19,8 +28,44 @@ import static java.nio.file.StandardCopyOption.REPLACE_EXISTING;
 @Service
 @Slf4j
 public class FileStorageServiceImpl implements FileStorageService {
-    private final String root = "src/main/uploads";
+    private String DOWNLOAD_URL = "https://firebasestorage.googleapis.com/v0/b/movie-1fb03.appspot.com/o/%s?alt=media";
+    private final String root = "uploads";
+    private String uploadFileToFireBase(File file, String fileName) throws IOException {
+        BlobId blobId = BlobId.of("movie-1fb03.appspot.com", fileName);
+        BlobInfo blobInfo = BlobInfo.newBuilder(blobId).setContentType(Files.probeContentType(file.toPath())).build();
+        ClassPathResource serviceAccount = new ClassPathResource("movie-1fb03-firebase-adminsdk-c1twk-7f70b5fa97.json");
+        Storage storage = StorageOptions.newBuilder().
+                setCredentials(GoogleCredentials.fromStream(serviceAccount.getInputStream())).
+                setProjectId("movie").build().getService();
+        storage.create(blobInfo, Files.readAllBytes(file.toPath()));
 
+        return String.format(DOWNLOAD_URL, URLEncoder.encode(fileName, String.valueOf(StandardCharsets.UTF_8)));
+    }
+
+    private File convertToFile(MultipartFile multipartFile, String fileName) throws IOException {
+        File tempFile = new File(fileName);
+        try (FileOutputStream fos = new FileOutputStream(tempFile)) {
+            fos.write(multipartFile.getBytes());
+        }
+        return tempFile;
+    }
+    @Override
+    public String save(MultipartFile multipartFile) {
+        try {
+            UUID uuid = UUID.randomUUID();
+            String uuidString = uuid.toString();
+            String ext = FilenameUtils.getExtension(multipartFile.getOriginalFilename());
+            String fileName = uuidString + "." + ext;                     // to get original file name
+            // to generated random string values for file name.
+            File file = this.convertToFile(multipartFile, fileName);                      // to convert multipartFile to File
+            String TEMP_URL = this.uploadFileToFireBase(file, fileName);
+            // to get uploaded file link
+            file.delete();                                                                // to delete the copy of uploaded file stored in the project folder
+            return fileName;                     // Your customized response
+        } catch (Exception e) {
+            throw new RuntimeException("Upload file unsuccessfully!");
+        }
+    }
     @Override
     public void init() {
         try {
@@ -33,21 +78,21 @@ public class FileStorageServiceImpl implements FileStorageService {
         }
     }
 
-    @Override
-    public String save(MultipartFile file) throws IOException {
-        try (InputStream inputStream = file.getInputStream()) {
-            UUID uuid = UUID.randomUUID();
-            String uuidString = uuid.toString();
-            String ext = FilenameUtils.getExtension(file.getOriginalFilename());
-            String fileName = uuidString + "." + ext;
-            ClassPathResource classPathResource = new ClassPathResource(root);
-            Path newPath = Paths.get(classPathResource.getURI()).resolve(fileName);
-            Files.copy(inputStream, newPath, REPLACE_EXISTING);
-            return fileName;
-        } catch (IOException e) {
-            throw new IOException("Could not save the file!");
-        }
-    }
+//    @Override
+//    public String save(MultipartFile file) throws IOException {
+//        try (InputStream inputStream = file.getInputStream()) {
+//            UUID uuid = UUID.randomUUID();
+//            String uuidString = uuid.toString();
+//            String ext = FilenameUtils.getExtension(file.getOriginalFilename());
+//            String fileName = uuidString + "." + ext;
+//            ClassPathResource classPathResource = new ClassPathResource(root);
+//            Path newPath = Paths.get(classPathResource.getURI()).resolve(fileName);
+//            Files.copy(inputStream, newPath, REPLACE_EXISTING);
+//            return fileName;
+//        } catch (IOException e) {
+//            throw new IOException("Could not save the file!");
+//        }
+//    }
 
     @Override
     public Resource load(String filename) {
